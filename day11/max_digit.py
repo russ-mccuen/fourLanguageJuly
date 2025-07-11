54b41874d4f8c110c9ba859ef1a59a7cf77ed0e9
Russ <russ.mccuen@gmail.com>
2025-07-11 18:50:32 -0400

    Update max_digit.py
    formatted print

--- day11/max_digit.py
+++ day11/max_digit.py
@@ -2,7 +2,7 @@ while True:
     user_input = input("Enter a number and I will extract the maximum digit: ")
     try:
         int(user_input)  # raises ValueError if not all digits
-        print(max(user_input))
+        print(f"The maximum digit is: {max(user_input)}")
         break
     except ValueError:
         print("Please enter only digits.")
